7ef5aa4d66e1af1ad1d6ab684534be732df21edb
Nourah Salem <65971542+Nourah-Salem@users.noreply.github.com>
2022-03-10 23:50:31 -0700

    Create EDA_sample_prep_test.py

---
+++ Unit Test/EDA_sample_prep_test.py
@@ -0,0 +1,14 @@
+# the unit test will be split into funtions that test every funtion within the EDA_sample_prep script
+# the tests include the correct inupts to every function, check all possible conditions on the functions bevahiour and check the correctness of the retrived output
+
+class EDA_sample_prep_test
+  def FASTA2DF_test (self):
+    # check the input (add other failing conditions
+    # check the filing condiitons for the folloing functions:
+    #.startswith
+    #.append
+    #.replace
+    #.concat
+    # check the resultant output for a correct output once and a failing condition
+    
+    
