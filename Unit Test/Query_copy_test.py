36cc988ba87e8495871c49d1cc4cfaa63c3a61e3
Nourah Salem <65971542+Nourah-Salem@users.noreply.github.com>
2022-03-10 23:52:42 -0700

    Create Query_copy_test.py

---
+++ Unit Test/Query_copy_test.py
@@ -0,0 +1,11 @@
+# the unit test will be split into funtions that test every funtion within the query_copy script
+# the tests include the correct inupts to every function, check all possible conditions on the functions bevahiour and check the correctness of the retrived output
+
+class EDA_sample_prep_test
+  def FASTA2DF_test (self):
+    # 1.	Inserting a short query that satisfy our function on a short list that has the query reads
+    #Inserting the actual data
+    # Insert a query that cannot be made by any read
+    #Insert an empty or different type query input
+    # Insert the right query but empty list of reads
+   
