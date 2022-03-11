03c2d6c9b6b57a419715789f0ea2fb37ff2d63ac
Nourah Salem <65971542+Nourah-Salem@users.noreply.github.com>
2022-03-11 00:00:41 -0700

    Update Query_copy_test.py

--- Unit Test/Query_copy_test.py
+++ Unit Test/Query_copy_test.py
@@ -1,11 +1,24 @@
 # the unit test will be split into funtions that test every funtion within the query_copy script
 # the tests include the correct inupts to every function, check all possible conditions on the functions bevahiour and check the correctness of the retrived output
 
-class EDA_sample_prep_test
-  def FASTA2DF_test (self):
-    # 1.	Inserting a short query that satisfy our function on a short list that has the query reads
-    #Inserting the actual data
+class Query_copy_test
+  
+  def LCS_test (self):  
+    # Inserting a short query that satisfy our function on a short list that has the query reads
+    # Inserting the actual data
     # Insert a query that cannot be made by any read
-    #Insert an empty or different type query input
+    # Insert an empty or different type query input
     # Insert the right query but empty list of reads
-   
+    
+   "also the script should test the following conditions"
+    # Reads are sorted before working on them
+    # The function flips the read and retry to align it again before jumping to the next read
+    # Print the overlapping parts of both the query and the read and their start/end indexes
+    # If more than 1 read fits, Store them in a list 
+    # Make sure that the found reads overlap with each other
+  
+  
+  def cut_query_test
+    # add reads that don't mact of cause cuts
+    # chec the loops of there is not match to the last piece of the query
+    
