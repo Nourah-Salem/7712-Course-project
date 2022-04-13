841d7b36ca0c8d16d0128b3b9fb21d9205aac3a6
Nourah Salem <65971542+Nourah-Salem@users.noreply.github.com>
2022-04-13 11:06:43 -0600

    Actual Testing Class

---
+++ Unit Test/testing_matching_range.py
@@ -0,0 +1,29 @@
+
+import unittest
+
+class TestPreprocessing(unittest.TestCase):
+    
+    def test_LCS_input (self):
+        a = 'ATGC'
+        b = "AATG"
+        self.assertEqual(type(a), str)
+        self.assertEqual(str, type(b))
+        
+        
+    def test_UpperCase(self):
+        x = "ATCG"
+        y = "GTCA"
+        self.assertTrue(x.isupper())
+        self.assertTrue(y.isupper())
+        
+        
+    def test_non_empty_input(self):
+        a = "AAA"
+        b = "ATCG"
+        self.assertNotEqual(len(a), len(""))
+        self.assertNotEqual(len(""),len(b))
+        
+if __name__ == '__main__':
+    unittest.main()
+        
+        
